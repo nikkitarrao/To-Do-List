db57b10efa032b6ddb3f0069a0974ad514d94a8f
nikkitarrao <144383426+nikkitarrao@users.noreply.github.com>
2023-10-11 23:24:30 -0400

    Update javascript.js

--- Project02/javascript.js
+++ Project02/javascript.js
@@ -1,6 +1,6 @@
 var arrayOfTasks = [];
 function click(){
-  alert("Are you sure you wanna add a new task")
+  alert("Are you sure you wanna add a new task");
 }
 document.querySelector("#new-task").onclick = function(){
   const line = document.createElement('li');
