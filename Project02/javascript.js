dfb103aad8599ec950e5850959c48e4b006b2269
nikkitarrao <144383426+nikkitarrao@users.noreply.github.com>
2023-10-12 18:44:35 -0400

    Update javascript.js

--- Project02/javascript.js
+++ Project02/javascript.js
@@ -1,20 +1,21 @@
 var arrayOfTasks = [];
 
 document.addEventListener('DOMContentLoaded', ()=> {
+  
   document.querySelector('#submit').disabled = true;
   document.querySelector('#task').onkeyup = () => {
       if(document.querySelector('#task').value.length > 0 )
         document.querySelector('#submit').disabled = false;
       else
         document.querySelector('#submit').disabled = true;
-  }
-  
-  
+  };
 document.querySelector("#new-task").onsubmit = () => {
   const li = document.createElement('li');
   line.innerHTML = document.querySelector('#task').value;
   document.querySelector('#tasks-list').append(li);
-  
-}
+  document.querySelector('#task').value = '';
+  document.querySelector('#submit').disabled = true;
+  return false; // why do i gotta return false?
+};
 
 });
