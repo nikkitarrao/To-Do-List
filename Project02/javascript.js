82b50e3af9fcc7c20bdcceef6c2cffce01e5550b
nikkitarrao <144383426+nikkitarrao@users.noreply.github.com>
2023-10-12 11:56:08 -0400

    Update javascript.js

--- Project02/javascript.js
+++ Project02/javascript.js
@@ -2,6 +2,12 @@ var arrayOfTasks = [];
 
 document.addEventListener('DOMContentLoaded', ()=> {
   document.querySelector('#submit').disabled = true;
+  document.querySelector('#task').onkeyup = () => {
+      if(document.querySelector('#task').value.length > 0 )
+        document.querySelector('#submit').disabled = false;
+      else
+        document.querySelector('#submit').disabled = true;
+  }
   
   
 document.querySelector("#new-task").onsubmit = () => {
