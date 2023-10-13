797040b0e1e5a8a39333b24639f70c6cd2ed15aa
nikkitarrao <144383426+nikkitarrao@users.noreply.github.com>
2023-10-12 22:14:01 -0400

    Update javascript.js

--- Project02/javascript.js
+++ Project02/javascript.js
@@ -14,7 +14,8 @@ document.querySelector("#new-task").onsubmit = () => {
   const li = document.createElement('li');
   let task_text = document.querySelector('#task').value;
   let new_task_html = `<span> ${task_text} </span>
-    <button class = "remove" > remove task from list </button>`;
+    <button class = "remove" > remove task from list </button>
+    <button class = "levelOfCompletion" > change completion status </button>`;
   
   li.innerHTML = new_task_html;
   document.querySelector('#tasks-list').append(li);
@@ -22,5 +23,15 @@ document.querySelector("#new-task").onsubmit = () => {
   document.querySelector('#submit').disabled = true;
   return false;
 };
+
+  document.addEventListener('click', function(event){
+    element = event.target;
+      if(element.className == 'remove'){
+          element.parentElement.remove();
+      }
+    if(element.className == 'levelOfCompletion'){
+          element.parentElement.style.textDecoration: 'line-through';
+      }
+  }
 
 });
