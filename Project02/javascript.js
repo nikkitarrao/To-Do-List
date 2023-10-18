124dfdf3358da40283e3f2b77a6e528e28adeda3
nikkitarrao <144383426+nikkitarrao@users.noreply.github.com>
2023-10-17 22:44:22 -0400

    Update javascript.js

--- Project02/javascript.js
+++ Project02/javascript.js
@@ -12,14 +12,14 @@ document.addEventListener('DOMContentLoaded', function() {
   //adding a task to the list
   document.querySelector("#new-task").onsubmit = () => {
     const li = document.createElement('li');
-    let taskText = document.querySelector('#task').value;
-    let newTaskHTML = ` `<span> ${taskText} </span>
-    <button class = "remove" > remove task from list </button> `;
-    li.innerHTML = newTaskHTML;
+  //  let taskText = document.querySelector('#task').value;
+   // let newTaskHTML = ` `<span> ${taskText} </span>
+   // <button class = "remove" > remove task from list </button> `;
+   // li.innerHTML = newTaskHTML;
 
-   // li.innerHTML = document.querySelector('#task').value;
-   // document.querySelector('#tasks-list').append(li);
-   // document.querySelector('#task').value = '';
+     li.innerHTML = document.querySelector('#task').value;
+     document.querySelector('#tasks-list').append(li);
+     document.querySelector('#task').value = '';
     return false;  
   }
 
