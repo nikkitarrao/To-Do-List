b3aee39e87590662b70de0eeeae522bc8a3023ca
nikkitarrao <144383426+nikkitarrao@users.noreply.github.com>
2023-12-07 18:10:30 -0500

    Update javascript.js

--- Project02/javascript.js
+++ Project02/javascript.js
@@ -15,7 +15,6 @@ document.addEventListener('DOMContentLoaded', function() {
      let taskPriority = document.querySelector('#task-priority').value;
      let newTaskHTML = `
                        <span> <p id = "task-title">task title: ${ taskText} </p> </span>
-                       <span id = "task-title"> <p>task title: ${ taskText} </p> </span>
                        <span> <p> task priority level: ${ taskPriority} </p>  </span>
                        <span> <p> task status:
                        <input type="radio" id="completed" name="task-status" class="completed" value="completed">
